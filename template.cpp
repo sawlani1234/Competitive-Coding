5fb63b075452cf88c1422f02db51fc90f7b9e48d
Shubham Sawlani <shubhamsawlani2006@gmail.com>
2020-12-22 01:46:20 +0530

    Adding header template

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define f(i, a, b) for (ll i = (ll)a; i <= (ll)b; i++)
+#define rf(i, a, b) for (ll i = (ll)a; i >= (ll)b; i--)
+#define po pop_back
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define debug(x) cout << #x << " = " << x << endl
+#define fast  ios_base::sync_with_stdio(false);cin.tie(0);
+#define all(x) x.begin(), x.end()
+#define PI 3.141592653
+#define endl "\n"
+using namespace std;
+/**
+  * @author :: Sawlani  
+  *
+  */
+
+const ll M = 1e9 + 7; // define MOD 
+
+template <typename T> // generalize debugging
+void print(T arr)
+{
+    for (auto i : arr)
+    {
+        cout  << i << " ";
+    }
+
+    cout << endl;
+}
